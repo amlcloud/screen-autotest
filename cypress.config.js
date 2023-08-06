d1a95447c9e60055991860eed37a5743b2c79af7
mounikavemunoori <mounikavemunoori@gmail.com>
2023-08-06 10:40:12 +0530

    Modified the cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    baseUrl: "http://localhost:8080",
+    baseUrl: "http://127.0.0.1:8080",
     defaultCommandTimeout:30000,
     numTestsKeptInMemory:100,
     chromeWebSecurity: false,
