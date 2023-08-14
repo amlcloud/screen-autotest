cee08c6c100540dc55c97284876cb9d006c6219b
mounikavemunoori <mounikavemunoori@gmail.com>
2023-08-14 14:20:49 +0530

    Added the intercept

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -31,21 +31,24 @@ export class LoginNewPage extends BrowserUtils {
     }
 
     navigateToAMLCloud() {
-        cy.request({
-            method: 'GET',
-            url: 'http://localhost:8080',
-            headers: {
-              'Connection': 'keep-alive',
-              'Content-Type': 'text/html',
-              'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-              'accept': '*/*',
-              'accept-encoding': 'gzip, deflate'
-              // Add more headers as needed
-            }
-          }).then((response) => {
-            // Add assertions or other test logic here
-            expect(response.status).to.eq(200); // Example assertion
-          });
+        // cy.request({
+        //     method: 'GET',
+        //     url: 'http://localhost:8080',
+        //     headers: {
+        //       'Connection': 'keep-alive',
+        //       'Content-Type': 'text/html',
+        //       'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        //       'accept': '*/*',
+        //       'accept-encoding': 'gzip, deflate'
+        //       // Add more headers as needed
+        //     }
+        //   }).then((response) => {
+        //     // Add assertions or other test logic here
+        //     expect(response.status).to.eq(200); // Example assertion
+        //   });
+        cy.intercept('GET', 'http://localhost:8080/**', (req) => {
+      req.headers['Content-Type'] = 'text/html';
+    }).as('customContentType');
           
         cy.visit(`${Cypress.config('baseUrl')}/#/login`, { failOnStatusCode: false })
         this.waitForDocumentExist()
